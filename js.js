87da6a4379ac0ad0e05b8e1500a5a95450ecd22f
LocTM <tranminhloc.anhk@gmail.com>
2023-12-02 22:49:13 +0700

    Chuyen do C sang do F

--- js.js
+++ js.js
@@ -5,3 +5,7 @@
 // let result = (a + b + c)/3
 // alert(result);
 
+// Thuật toán chuyển đổi nhiệt độ
+// let a = prompt("Nhap vao do C: ")
+// let result = a * 9 / 5 +32;
+// alert (`${a} do C bang : ${result} do F`)
