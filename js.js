1e40c4e5fbc6bfceaf301cec16073e3e6ee72807
LocTM <tranminhloc.anhk@gmail.com>
2023-12-02 23:35:43 +0700

    tim max trong 3 so

--- js.js
+++ js.js
@@ -9,3 +9,16 @@
 // let a = prompt("Nhap vao do C: ")
 // let result = a * 9 / 5 +32;
 // alert (`${a} do C bang : ${result} do F`)
+
+// //Tìm giá trị lớn nhất trong 1 dãy số n
+// let a = +prompt("Nhập vào đây số a:" )
+// let b = +prompt("Nhập vào đây số b:" )
+// let c = +prompt("Nhập vào đây số c:" )
+// let max = a;
+// if( b > max){
+//     max = b;
+// }
+// if ( c > max){
+//     max = c;
+// }
+// alert(max);
